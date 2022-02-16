1b765aacb1f3e4ee6ed34cb98b1d7ae42c6c977c
nurgnc <gencbedriyenur@gmail.com>
2022-02-17 01:02:00 +0300

    Update Nav.jsx

--- src/components/base/Nav.jsx
+++ src/components/base/Nav.jsx
@@ -7,7 +7,7 @@ function Nav() {
   return (
     <HStack spacing='24px' p={5} bg='lightseagreen' >
       <Container maxW='container.xl' display='flex' alignItems='center' justifyContent="space-between">
-        <Image width={'80px'} src={logo} alt='logo' />
+        <Link to="/"><Image width={'80px'} src={logo} alt='logo' /></Link>
         <Box display={{ lg: 'flex', md: 'flex' }} alignContent='center'>
           <Link to="/"><Text fontSize='lg' color='white' >&#10002; BMI Calculate</Text></Link>
           <Link to="/about"><Text mx={{ lg: '6' }} fontSize='lg' color='white' >🔎 What's the BMI?</Text></Link>
